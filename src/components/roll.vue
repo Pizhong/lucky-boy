d9d91a513f26a54757dea3717158fd7162735411
Pizhong <15626160293@163.com>
2021-09-01 18:56:06 +0800

    滚动组件初始化

---
+++ src/components/roll.vue
@@ -0,0 +1,68 @@
+<!--
+ * @file: 滚动
+-->
+<template>
+  <div class="c_roll">
+    <ul>
+      <li v-for="(item,index) in dataList" :key="index" ref="rolling" class="c_roll_list-item" :class="{'c_roll_list-item-animate':isAnimate == true}">{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Roll',
+  data(){
+    return{
+      dataList: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+      timer: null,
+      isAnimate: true,
+    }
+  },
+  mounted(){
+    this.timer = setInterval(() => {
+      this.scrollAnimate()
+    }, 1000);
+  },
+  destroyed(){
+    clearInterval(this.timer)
+  },
+  methods: {
+    scrollAnimate(){
+      let rol = this.$refs.rolling
+      rol[0].style.marginTop = '30px'
+      this.isAnimate = !this.isAnimate
+      setTimeout(()=>{
+        this.dataList.push(this.dataList[0])
+        this.dataList.shift()
+        rol[0].style.marginTop = '0px'
+        this.isAnimate = !this.isAnimate
+      },0)
+    },
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.c_roll{
+  margin: 100px auto;
+  overflow: hidden;
+}
+.c_roll ul{
+  height: 40px;
+  overflow: hidden;
+}
+.c_roll_list-item{
+  width: 100%;
+  list-style: none;
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+  font-size: 24px;
+  font-weight: 400;
+}
+.c_roll_list-item-animate{
+  transition: all 0.1s ease;
+}
+
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,21 @@
+<!--
+ * @file: 
+-->
+<template>
+  <div>
+    <roll></roll>
+  </div>
+</template>
+
+<script>
+import Roll from '../components/roll.vue'
+export default {
+  components:{
+    Roll
+  }
+}
+</script>
+
+<style>
+
+</style>
